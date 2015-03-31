0528f368c992cc323bfe26219ef1a4cd3634cc25
Chris Tweedie <cltweedie@gmail.com>
2015-03-31 18:49:54 +0100

    Updated about page and gallery descriptions as requested.

--- js/galleries.js
+++ js/galleries.js
@@ -1,11 +1,27 @@
 var galleries = [
 {
 	name: 'Balata',
-	description: "Balata Refugee Camp project description goes here.",
+	description: "Balata is a refugee camp in the town of Nablus in the West Bank.\
+	The site is still administrated by UNRWA, the United Nations Relief and Works Agency,\
+	whose sole purpose is to assist and protect Palestinian refugees.\
+	Balata started life as a typical tented camp but now has developed in to a crowded, winding neighbourhood. \
+	There is a high unemployment rate and 40% of the population are under 15. \
+	However the people are incredibly friendly and hospitable, the kids seem carefree, \
+	but a lot of them have injuries, that their elders like to claim are due to Israeli incursions. \
+	Posters of martyrs hang off bullet-riddled walls, and many in the neighbourhood have fought for Hamas and other groups. \
+	Almost everyone has lost relatives to fighting. The IDF presence is not as strong as it has been, \
+	but they have created another generation that will despise them.<br>",
 	images: 12
 },
 {
 	name: 'Zambia',
-	description: "Zambia project description goes here.",
+	description: "I spent three months documenting the work of Eventure Zambia. \
+	An NGO set up with the purpose of building a school for Zambian teenagers. \
+	I arrived on an empty beach on the shore of Lake Tanganyika, there was nothing \
+	but a couple of old clay huts and a tent to serve as our kitchen. There were villages \
+	nearby but the nearest significant centre was 50 minutes away by boat.  A combination of \
+	Danish volunteers and local workers set about turning this forgotten corner of a forgotten \
+	country into a special place for Zambian teenagers to realise their potential.\
+	<br><br>Check out my videos page for the promo film.<br>",
 	images: 38
 }];
